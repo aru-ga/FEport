e60f1fabd0970afd4e84654d138498a3c7ecbb81
aru-ga <mfargazzy@gmail.com>
2023-07-01 02:15:35 +0700

    fixed dark mode and navbar
    still has bug with dark mode since its need to interact with other elements such as navbar, card, etc

--- frontend/script/script.js
+++ frontend/script/script.js
@@ -1,20 +1,14 @@
 window.addEventListener('scroll', function() {
-    let navbar = $('.header-bg');
-    
-    if (window.scrollY > 0) {
-      navbar.classList.add('sticky-nav');
-    } else {
-      navbar.classList.remove('sticky-nav');
-    }
-  });
-  
-// $(".dm-switch").on("click", function(){
-//   if ($("body").hasClass("dm")){
-//       $("body").removeClass("dm");
-//       $(".dm-switch").text("terangin aj bg");
-//   }
-//   else{
-//     $("body").addClass("dm");
-//     $(".dm-switch").text("terangin aj bg");
-//   }
-// })
+  let navbar = document.querySelector('.nav-links'); 
+
+  if (window.scrollY > 0) {
+    navbar.classList.add('nav-dark');
+  } else {
+    navbar.classList.remove('nav-dark');
+  }
+});
+
+function darkMode(){
+  let bodie = document.body;
+  bodie.classList.toggle("dm");
+}
