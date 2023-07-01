565d5b8ec916b6f06a0ab2afb0e405779dababfe
aru-ga <mfargazzy@gmail.com>
2023-07-01 17:54:19 +0700

    fix dark mode in navbar scroll

--- frontend/script/script.js
+++ frontend/script/script.js
@@ -1,10 +1,16 @@
 let navbarCollection = document.getElementsByClassName('linkes');
 
 window.addEventListener('scroll', function() {
-  for (let i = 0; i < navbarCollection.length; i++) {
-    if (window.scrollY > 0) {
-      navbarCollection[i].classList.add('nav-dark');
-    } else {
+  if (!document.body.classList.contains('dm')) {
+    for (let i = 0; i < navbarCollection.length; i++) {
+      if (window.scrollY > (window.innerHeight * 0.8)) {
+        navbarCollection[i].classList.add('nav-dark');
+      } else {
+        navbarCollection[i].classList.remove('nav-dark');
+      }
+    }
+  } else {
+    for (let i = 0; i < navbarCollection.length; i++) {
       navbarCollection[i].classList.remove('nav-dark');
     }
   }
@@ -14,6 +20,9 @@ window.addEventListener('scroll', function() {
 
 function toggleDarkMode() {
   document.body.classList.toggle('dm');
-  let navLinks = document.getElementById('nav-links');
-  navLinks.classList.toggle('nav-dark');
+  let navLinks = document.getElementsByClassName('linkes');
+
+  for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].classList.toggle('nav-dark');
+  }
 }
