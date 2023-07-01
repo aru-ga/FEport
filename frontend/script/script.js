1d3b0e2cb984b4764022a0ef5578e5ee0fae0ce2
aru-ga <mfargazzy@gmail.com>
2023-07-01 17:08:29 +0700

    code efficiences

--- frontend/script/script.js
+++ frontend/script/script.js
@@ -1,14 +1,19 @@
-window.addEventListener('scroll', function() {
-  let navbar = document.querySelector('.nav-links'); 
+let navbarCollection = document.getElementsByClassName('linkes');
 
-  if (window.scrollY > 0) {
-    navbar.classList.add('nav-dark');
-  } else {
-    navbar.classList.remove('nav-dark');
+window.addEventListener('scroll', function() {
+  for (let i = 0; i < navbarCollection.length; i++) {
+    if (window.scrollY > 0) {
+      navbarCollection[i].classList.add('nav-dark');
+    } else {
+      navbarCollection[i].classList.remove('nav-dark');
+    }
   }
 });
 
-function darkMode(){
-  let bodie = document.body;
-  bodie.classList.toggle("dm");
+
+
+function toggleDarkMode() {
+  document.body.classList.toggle('dm');
+  let navLinks = document.getElementById('nav-links');
+  navLinks.classList.toggle('nav-dark');
 }
